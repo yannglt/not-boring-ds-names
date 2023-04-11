3b3ae7d4617bda2414bc0291bad10abb6df4da88
Yann-Edern Gillet <helloyannglt@gmail.com>
2023-04-11 10:21:30 +0200

    Add stream option and vercel edge function

---
+++ components/Header.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import styles from "../styles/Header.module.css";
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.title}>
+        <h1 className="body-2-l text-high-emphasis">Not Boring Design System Names</h1>
+        <p className="body-2-s text-medium-emphasis">by</p>
+        <Image
+          alt="Specify Logo"
+          src="/images/specify-logo.svg"
+          className={styles.logo}
+          width={76}
+          height={24}
+        />
+        {/* <img className={styles.logo} src="/images/specify-logo.svg"/> */}
+      </div>
+      <ul className={styles.list}>
+        <li className={styles.listItem}><a href="/" className="body-2-m text-medium-emphasis">Index</a></li>
+        <li className={styles.listItem}><a href="/about" className="body-2-m text-medium-emphasis">About</a></li>
+      </ul>
+    </header>
+  );
+}
