fa4d7a2ec17f958abd326b9018ff0d13d870bb96
Yann-Edern Gillet <helloyannglt@gmail.com>
2023-04-13 17:35:46 +0200

    Add footer

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,14 +8,15 @@ export default function Header() {
       <div className={styles.title}>
         <h1 className="body-2-l text-high-emphasis">Not Boring Design System Names</h1>
         <p className="body-2-s text-medium-emphasis">by</p>
-        <Image
-          alt="Specify Logo"
-          src="/images/specify-logo.svg"
-          className={styles.logo}
-          width={76}
-          height={24}
-        />
-        {/* <img className={styles.logo} src="/images/specify-logo.svg"/> */}
+        <Link href="https://specifyapp.com" target="_blank">
+          <Image
+            alt="Specify Logo"
+            src="/images/specify-logo.svg"
+            className={styles.logo}
+            width={76}
+            height={24}
+          />
+        </Link>
       </div>
       <ul className={styles.list}>
         <li className={styles.listItem}>
@@ -23,6 +24,9 @@ export default function Header() {
         </li>
         <li className={styles.listItem}>
           <Link href="/" className="body-2-m text-medium-emphasis">About</Link>
+        </li>
+        <li className={styles.listItem}>
+          <Link href="/" className="body-2-m text-medium-emphasis">Share</Link>
         </li>
       </ul>
     </header>
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { useState } from "react";
 
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 import Animation from "../components/Animation";
 
 import hero from "../styles/Hero.module.css";
@@ -86,70 +87,71 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={loading ? "loading" : ""}>
+      <div className="container">
         <Header />
-        <div className={hero.hero}>
-          <p className="heading-1 text-high-emphasis">Empower your design system<span className={hero.icon + " " + hero.designSystem}><Animation path="design-system.json" className={hero.animation} /></span>with a memorable name<span className={hero.icon + " " + hero.name}><Animation path="name.json" className={hero.animation} /></span>that conveys emotions<span className={hero.icon + " " + hero.emotions}><Animation path="emotions.json" className={hero.animation} /></span>, and extends your brand characteristics<span className={hero.icon + " " + hero.brandCharacteristics}><Animation path="brand-characteristics.json" className={hero.animation} /></span>.</p>
-          <p className="body-1-s text-low-emphasis">Your design system is the backbone of your brand&apos;s visual identity, but a great name can take it to the next level. With the power of GPT-3, we&apos;ll help you generate a name that captures your brand&apos;s unique characteristics, industry, and personality.</p>
-        </div>
-
-        <div className={form.container}>
-          <form className={form.form} onSubmit={generateName} autoComplete="off">
-            <div className={form.group}>
-              <label htmlFor="companyName" className="body-2-l text-high-emphasis">Company name</label>
-              <input type="text" id="companyName" name="companyName" className="body-2-m text-medium-emphasis" placeholder="NASA" onChange={(event) => setCompanyName(event.target.value)} />
-            </div>
-            <div className={form.group}>
-              <label htmlFor="brandCharacteristics" className="body-2-l text-high-emphasis">Brand characteristics</label>
-              <input type="select" id="brandCharacteristics" name="brandCharacteristics" className="body-2-m text-medium-emphasis" placeholder="Creative" onChange={(event) => setBrandCharacteristics(event.target.value)} />
-            </div>
-            <div className={form.group}>
-              <label htmlFor="space" className="body-2-l text-high-emphasis">Space</label>
-              <select name="space" id="space" className="body-2-m text-medium-emphasis" onChange={(event) => setSpace(event.target.value)}>
-                <option value="automotive">Automotive</option>
-                <option value="banking">Banking</option>
-                <option value="construction">Construction</option>
-                <option value="education">Education</option>
-                <option value="energy">Energy</option>
-                <option value="entertainment">Entertainment</option>
-                <option value="finance">Finance</option>
-                <option value="food">Food</option>
-                <option value="healthcare">Healthcare</option>
-                <option value="hospitality">Hospitality</option>
-                <option value="insurance">Insurance</option>
-                <option value="manufacturing">Manufacturing</option>
-                <option value="media">Media</option>
-                <option value="real-estate">Real Estate</option>
-                <option value="retail">Retail</option>
-                <option value="technology">Technology</option>
-                <option value="telecommunications">Telecommunications</option>
-                <option value="transportation">Transportation</option>
-                <option value="travel">Travel</option>
-                <option value="utilities">Utilities</option>
-              </select>
-            </div>
-            <button className="button-primary" type="submit" disabled={loading}>Generate a name 
-              <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M8.11284 5.64927C6.84903 5.85991 5.85868 6.85025 5.64805 8.11406C5.62021 8.2811 5.38012 8.2811 5.35228 8.11406C5.14164 6.85025 4.15129 5.85991 2.88749 5.64927C2.72045 5.62143 2.72045 5.38134 2.88749 5.3535C4.15129 5.14286 5.14164 4.15251 5.35228 2.88871C5.38012 2.72167 5.62021 2.72167 5.64805 2.88871C5.85868 4.15251 6.84903 5.14286 8.11284 5.3535C8.27988 5.38134 8.27988 5.62143 8.11284 5.64927Z" fill="white"/>
-                <path d="M4.71974 10.5198L7.08349 9.85799C8.42882 9.48129 9.48013 8.42998 9.85683 7.08465L10.5187 4.7209C10.655 4.23399 11.3453 4.23398 11.4816 4.7209L12.1435 7.08465C12.5202 8.42998 13.5715 9.48129 14.9168 9.85799L17.2806 10.5198C17.7675 10.6562 17.7675 11.3465 17.2806 11.4828L14.9168 12.1447C13.5715 12.5213 12.5202 13.5727 12.1435 14.918L11.4816 17.2817C11.3453 17.7687 10.655 17.7687 10.5187 17.2817L9.85683 14.918C9.48013 13.5727 8.42882 12.5213 7.08349 12.1447L4.71974 11.4828C4.23283 11.3465 4.23282 10.6562 4.71974 10.5198Z" fill="white"/>
-              </svg>
-            </button>
-          </form>
-          <div className={form.result}>
-            {displayedName && !loading
-              ? <p className="display-1 text-high-emphasis text-result">{displayedName}<br />Design System</p>
-              : <p className="display-1 text-low-emphasis text-disabled">Your design system name</p>
-            }
-
-            {displayedExplanation && !loading
-              ? <p className="body-emphasis text-low-emphasis">{displayedExplanation}</p>
-              : <p className="body-emphasis text-low-emphasis">You&apos;ll find the perfect description for your design system</p>
-            }
+        <main className={loading ? "loading" : ""}>
+          <div className={hero.hero}>
+            <p className="heading-1 text-high-emphasis">Empower your design system<span className={hero.icon + " " + hero.designSystem}><Animation path="design-system.json" className={hero.animation} /></span>with a memorable name<span className={hero.icon + " " + hero.name}><Animation path="name.json" className={hero.animation} /></span>that conveys emotions<span className={hero.icon + " " + hero.emotions}><Animation path="emotions.json" className={hero.animation} /></span>, and extends your brand characteristics<span className={hero.icon + " " + hero.brandCharacteristics}><Animation path="brand-characteristics.json" className={hero.animation} /></span>.</p>
+            <p className="body-1-s text-low-emphasis">Your design system is the backbone of your brand&apos;s visual identity, but a great name can take it to the next level. With the power of GPT-3, we&apos;ll help you generate a name that captures your brand&apos;s unique characteristics, industry, and personality.</p>
           </div>
-        </div>
-        {/* <button onClick={(e) => generateName(e)}>Generate your name &rarr;</button>
-        { loading ? <p>Loading...</p> : <p>{generatedName}</p> } */}
-      </main>
+
+          <div className={form.container}>
+            <form className={form.form} onSubmit={generateName} autoComplete="off">
+              <div className={form.group}>
+                <label htmlFor="companyName" className="body-2-l text-high-emphasis">Company name</label>
+                <input type="text" id="companyName" name="companyName" className="body-2-m text-medium-emphasis" placeholder="NASA" onChange={(event) => setCompanyName(event.target.value)} />
+              </div>
+              <div className={form.group}>
+                <label htmlFor="brandCharacteristics" className="body-2-l text-high-emphasis">Brand characteristics</label>
+                <input type="select" id="brandCharacteristics" name="brandCharacteristics" className="body-2-m text-medium-emphasis" placeholder="Creative" onChange={(event) => setBrandCharacteristics(event.target.value)} />
+              </div>
+              <div className={form.group}>
+                <label htmlFor="space" className="body-2-l text-high-emphasis">Space</label>
+                <select name="space" id="space" className="body-2-m text-medium-emphasis" onChange={(event) => setSpace(event.target.value)}>
+                  <option value="automotive">Automotive</option>
+                  <option value="banking">Banking</option>
+                  <option value="construction">Construction</option>
+                  <option value="education">Education</option>
+                  <option value="energy">Energy</option>
+                  <option value="entertainment">Entertainment</option>
+                  <option value="finance">Finance</option>
+                  <option value="food">Food</option>
+                  <option value="healthcare">Healthcare</option>
+                  <option value="hospitality">Hospitality</option>
+                  <option value="insurance">Insurance</option>
+                  <option value="manufacturing">Manufacturing</option>
+                  <option value="media">Media</option>
+                  <option value="real-estate">Real Estate</option>
+                  <option value="retail">Retail</option>
+                  <option value="technology">Technology</option>
+                  <option value="telecommunications">Telecommunications</option>
+                  <option value="transportation">Transportation</option>
+                  <option value="travel">Travel</option>
+                  <option value="utilities">Utilities</option>
+                </select>
+              </div>
+              <button className="button-primary" type="submit" disabled={loading}>Generate a name 
+                <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M8.11284 5.64927C6.84903 5.85991 5.85868 6.85025 5.64805 8.11406C5.62021 8.2811 5.38012 8.2811 5.35228 8.11406C5.14164 6.85025 4.15129 5.85991 2.88749 5.64927C2.72045 5.62143 2.72045 5.38134 2.88749 5.3535C4.15129 5.14286 5.14164 4.15251 5.35228 2.88871C5.38012 2.72167 5.62021 2.72167 5.64805 2.88871C5.85868 4.15251 6.84903 5.14286 8.11284 5.3535C8.27988 5.38134 8.27988 5.62143 8.11284 5.64927Z" fill="white"/>
+                  <path d="M4.71974 10.5198L7.08349 9.85799C8.42882 9.48129 9.48013 8.42998 9.85683 7.08465L10.5187 4.7209C10.655 4.23399 11.3453 4.23398 11.4816 4.7209L12.1435 7.08465C12.5202 8.42998 13.5715 9.48129 14.9168 9.85799L17.2806 10.5198C17.7675 10.6562 17.7675 11.3465 17.2806 11.4828L14.9168 12.1447C13.5715 12.5213 12.5202 13.5727 12.1435 14.918L11.4816 17.2817C11.3453 17.7687 10.655 17.7687 10.5187 17.2817L9.85683 14.918C9.48013 13.5727 8.42882 12.5213 7.08349 12.1447L4.71974 11.4828C4.23283 11.3465 4.23282 10.6562 4.71974 10.5198Z" fill="white"/>
+                </svg>
+              </button>
+            </form>
+            <div className={form.result}>
+              {displayedName && !loading
+                ? <p className="display-1 text-high-emphasis text-result">{displayedName}<br />Design System</p>
+                : <p className="display-1 text-low-emphasis text-disabled">Your design system name</p>
+              }
+
+              {displayedExplanation && !loading
+                ? <p className="body-emphasis text-low-emphasis">{displayedExplanation}</p>
+                : <p className="body-emphasis text-low-emphasis">You&apos;ll find the perfect description for your design system</p>
+              }
+            </div>
+          </div>
+        </main>
+        <Footer />
+      </div>
     </>
   )
 }
