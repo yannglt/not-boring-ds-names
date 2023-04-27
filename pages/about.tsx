7b39e29c95a5f556d338ac4d47273f6a4819abc9
Yann-Edern Gillet <helloyannglt@gmail.com>
2023-04-27 15:21:42 +0200

    Fix weights use on about page titles

--- pages/about.tsx
+++ pages/about.tsx
@@ -33,17 +33,17 @@ export default function About() {
             >
               <div className={about.story}>
                 <div className={about.storyPart}>
-                  <h2 className="body-2-l text-low-emphasis">Initial inspiration</h2>
+                  <h2 className="body-2-m text-low-emphasis">Initial inspiration</h2>
                   <p className="body-1-s text-medium-emphasis">During our 2023 Winter Retreat in January, during our hackathon day, we think about how AGI can help teams working with/on design systems. A lot of topics were mentioned, and we thought about <Link className="text-info" href="https://designtitles.com/" target="_blank">Design Titles ↗︎</Link>, a playful website created by Xtian Miller and Boris Crowther where you can generate job names for designers.</p>
                   <p className="body-1-s text-medium-emphasis">We loved this idea of generating names, and we know from experience naming stuff, whether we talk about tokens, components, or even worse — your layers, can sometimes be tricky. So we chose to build a tool that provides ideas for teams looking for a name for their design systems.</p>
                 </div>
                 <div className={about.storyPart}>
-                  <h2 className="body-2-l text-low-emphasis">Proof of Concept</h2>
+                  <h2 className="body-2-m text-low-emphasis">Proof of Concept</h2>
                   <p className="body-1-s text-medium-emphasis">Very often, design systems tend to be named &#123;companyName&#125; UI, &#123;companyName&#125; Design System, &#123;companyName&#125; DLS. While this is totally fine, we were wondering if GPT-3 could generate appropriate names based on the company name, its brand characteristics and its space. In addition, we wanted a name that could make sense for every company and its design system.</p>
                   <p className="body-1-s text-medium-emphasis">After playing a bit with ChatGPT to see if that could be interesting, we created a proof of concept resulting in a simple Next JS app with three input fields, a button, and a result displayed in a way too big font size, set in Inter (of course).</p>
                 </div>
                 <div className={about.storyPart}>
-                  <h2 className="body-2-l text-low-emphasis">Final product</h2>
+                  <h2 className="body-2-m text-low-emphasis">Final product</h2>
                   <p className="body-1-s text-medium-emphasis">During our cooldown week of April, Yann-Edern took the initiative to transform this POC into something that can be shared on the Internets. He also made it more on-brand, improved the prompt sent to GPT-3, and leveraged Vercel Edge Functions, thanks to <Link className="text-info" href="https://vercel.com/blog/gpt-3-app-next-js-vercel-edge-functions" target="_blank">this brilliant article ↗︎</Link> from Hassan El Mghari.</p>
                 </div>
               </div>
@@ -54,7 +54,7 @@ export default function About() {
                 transition={{ ease: "easeInOut", duration: 0.4, delay: 0.4 }}
               >
                 <div className={about.information}>
-                  <p className="body-2-l text-low-emphasis">Makers</p>
+                  <p className="body-2-m text-low-emphasis">Makers</p>
                   <div className={about.makers}>
                     <Maker name="Yann-Edern Gillet" role="Product Designer" link="" path="yann.jpg" />
                     <Maker name="Mohamed Khalil Elloumi" role="Back-End Engineer" link="" path="khalil.jpg" />
@@ -64,7 +64,7 @@ export default function About() {
                 </div>
                 <div className={about.information}>
                   <div className={about.stackTitle}>
-                    <p className="body-2-l text-low-emphasis">Stack</p>
+                    <p className="body-2-m text-low-emphasis">Stack</p>
                     <Link className="body-2-s text-info" href="https://github.com/yannglt/not-boring-ds-names" target="_blank">View code ↗︎</Link>
                   </div>
                   <div className={about.stackList}>
@@ -87,9 +87,9 @@ export default function About() {
                   </div>
                 </div>
                 <div className={about.information}>
-                  <p className="body-2-l text-low-emphasis">About Specify</p>
+                  <p className="body-2-m text-low-emphasis">About Specify</p>
                   <div className={about.logo}></div>
-                  <p className="body-2-s text-low-emphasis">Specify is a design system infrastructure for modern teams. With Specify, product teams release better experiences faster while staying in-line with their company standards.</p>
+                  <p className="body-2-s text-medium-emphasis">Specify is a design system infrastructure for modern teams. With Specify, product teams release better experiences faster while staying in-line with their company standards.</p>
                   <Link className="button button-medium button-secondary" href="https://specifyapp.com" target="_blank">Learn more
                     <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                       <path d="M11.3 3.75146C11.3 4.16568 11.6358 4.50146 12.05 4.50146H14.4393L8.46967 10.4711C8.17678 10.764 8.17678 11.2389 8.46967 11.5318C8.76256 11.8247 9.23744 11.8247 9.53033 11.5318L15.5 5.56213V7.95146C15.5 8.36568 15.8358 8.70146 16.25 8.70146C16.6642 8.70146 17 8.36568 17 7.95146V3.75146C17 3.33725 16.6642 3.00146 16.25 3.00146H12.05C11.6358 3.00146 11.3 3.33725 11.3 3.75146Z" />
